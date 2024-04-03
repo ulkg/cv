08883ebc06e551b6b29b77f4bf9e9ca585534d67
ulkg <ulrich.gram@protonmail.com>
2024-04-03 21:12:21 +0200

    Updates

---
+++ precache-manifest.58dc0586f81723f82f80b1b8de291f0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983dc2bc42bacf59bcd163e06516a44d",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "de03fef26135a75b3aaf",
+    "url": "/cv/static/css/2.9ae757e2.chunk.css"
+  },
+  {
+    "revision": "a269122514f0d081e292",
+    "url": "/cv/static/css/main.9cd8ede0.chunk.css"
+  },
+  {
+    "revision": "de03fef26135a75b3aaf",
+    "url": "/cv/static/js/2.26813efc.chunk.js"
+  },
+  {
+    "revision": "ac12dcf4950ae785dd3c2a2ab3ca2052",
+    "url": "/cv/static/js/2.26813efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a269122514f0d081e292",
+    "url": "/cv/static/js/main.f8f9e4af.chunk.js"
+  },
+  {
+    "revision": "f7bf87fc937b60eafa84",
+    "url": "/cv/static/js/runtime-main.b0e2fc26.js"
+  },
+  {
+    "revision": "e43ccc4fd6ce0c0c207b112da1df7549",
+    "url": "/cv/static/media/photo.e43ccc4f.png"
+  }
+]);
